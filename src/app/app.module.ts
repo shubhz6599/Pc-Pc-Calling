ab8a4f04a962fa292727c39ec668f7e34c169d0d
Shubham Karkhile <shubhz6599@gmail.com>
2025-09-15 11:58:01 +0530

    Ui Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SupplierCallComponent } from './supplier-call/supplier-call.component';
 import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
